e96f06128ef99556f6429457b10adc2508df0dcb
Troy Chavkin <tachavkin@gmail.com>
2022-11-04 17:05:10 -0600

    Console log any key

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function buttonClick() {
-    console.log(Math.random())
+
+//button click console logs the console, which tells you what is in the console.
+
+function buttonClick(event) {
+    console.log(console)
 }
+
+//press and key and have it desplay in the console
+
+document.addEventListener('keydown', (event) => console.log(event.key))
+
+
+
