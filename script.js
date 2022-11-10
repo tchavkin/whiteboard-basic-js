0c107ffd81f866b081c84ec1ee8a8e4b125dd417
Troy Chavkin <tachavkin@gmail.com>
2022-11-09 17:09:20 -0700

    cleanup

--- script.js
+++ script.js
@@ -2,15 +2,8 @@
 document.addEventListener('keydown', (event) => 
 document.getElementById("display-p").innerHTML = event.key)
 
-
-
-//button click console logs the console, which tells you what is in the console.
-
-// function buttonClick(event) {
 //     console.log(console)
-// }
 
-//press and key and have it desplay in the console
 
 
 
